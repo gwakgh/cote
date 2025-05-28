2997f492b4a7efa586233c7b4ea90b56a0ece4e7
gwakgh <104366376+gwakgh@users.noreply.github.com>
2025-05-28 10:13:38 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.c
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -5,17 +5,9 @@
 int solution(int n) {
     int answer = 0;
     
-    printf("%d의 약수는 ");
-        
-    for(int i = 1; i <= n; i++){
-        if (n % i == 0) {
-            printf("%d", i);
-            if(i != n) {
-                printf(", ");
-            }
+    for(int i = 1; i <= n; i++)
+        if (n % i == 0) 
             answer += i;
-        }
-    }
-    printf("입니다. 이를 모두 더하면 %d입니다.\n", answer);
+    
     return answer;
 }
